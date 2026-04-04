fa68934eac76da1a7c215c2b0254b4bfea1e8ef6
Lisa Copeland <lisacopelandswe@gmail.com>
2026-04-04 17:26:21 -0400

    feat: updated logo and favicon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import logo from "../../public/dtp-logo-no-text.svg";
+import logo from "../../public/dtp-updated-logo.svg";
 
 export default function Navbar() {
   return (
